3ff792871bc07071c8bcb557cf80f773cf1a7abd
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2014-03-31 01:49:32 +0200

    Renaming of some functions

--- files/hbase-remote-hard-stop.sh
+++ files/hbase-remote-hard-stop.sh
@@ -2,4 +2,4 @@
 # $1: which service (regionserver/master)
 # $2: What? (KILL/...)
 # $3: where
-echo "kill -$2 $(cat /var/run/hbase/hbase-hbase-$1.pid)" | ssh $3
+echo "kill -$2 \$(cat /var/run/hbase/hbase-hbase-$1.pid)" | ssh $3
--- files/mongodb-remote-hard-stop.sh
+++ files/mongodb-remote-hard-stop.sh
@@ -2,4 +2,4 @@
 # $1: which service (mongod/mongodb-accessserver/mongodb-configserver)
 # $2: What? (KILL/...)
 # $3: where
-echo "kill -$2 $(cat /var/run/mongodb/$1.pid)" | ssh $3
+echo "kill -$2 \$(cat /var/run/mongodb/$1.pid)" | ssh $3
